5d0662c7e825f9d6df66669cadeaa9a763291a91
Adimov, Denis <CSTADIMOD@CORP.JTI.COM>
2021-05-07 23:11:43 +0300

    scan storage implementation

---
+++ DuplicateScanner.Cmd/Assets/Scanners/ScannerMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DuplicateScanner.Cmd.Assets.Scanners
+{
+    [Flags]
+    enum ScannerMode
+    {
+        None = 0,
+        SHA256 = 1,
+        MD5 = 2,
+        Bytes = 4
+    }
+}
---
+++ DuplicateScanner.Cmd/Assets/Scanners/ScannerStorage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using DuplicateScanner.Cmd.Assets.Scanners.Modes;
+
+namespace DuplicateScanner.Cmd.Assets.Scanners
+{
+    static class ScannerStorage
+    {
+        private static readonly ScannerMode[] Modes;
+
+        private static readonly Dictionary<ScannerMode, Func<IFileScanner>> Storage = new()
+        {
+            {
+                ScannerMode.MD5,
+                () => new Md5Scanner()
+            },
+            {
+                ScannerMode.SHA256,
+                () => new Sha256Scanner()
+            },
+            {
+                ScannerMode.Bytes,
+                () => new ByteScanner()
+            }
+        };
+
+        static ScannerStorage()
+        {
+            Modes = Enum.GetValues<ScannerMode>();
+        }
+
+        public static IEnumerable<IFileScanner> GetScanners(ScannerMode selectedMode)
+        {
+            foreach (var mode in Modes)
+            {
+                var key = selectedMode & mode;
+                if (Storage.TryGetValue(key, out var func))
+                    yield return func();
+            }
+        }
+    }
+}
