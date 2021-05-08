a37d072be69fe0389cc8310c4926753a5011a2fa
Adimov, Denis <CSTADIMOD@CORP.JTI.COM>
2021-05-08 13:08:57 +0300

    added DI implementation & usage

--- DuplicateScanner.Cmd/Program.cs
+++ DuplicateScanner.Cmd/Program.cs
@@ -1,12 +1,33 @@
-﻿using DuplicateScanner.Cmd.UI;
+﻿using DuplicateScanner.Cmd.Extensions;
+using DuplicateScanner.Cmd.UI;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace DuplicateScanner.Cmd
 {
     internal class Program
     {
+        private static IServiceProvider Provider { get; }
+
+        private static IUserInterface UserInterface { get; }
+        
+        static Program()
+        {
+            var collection = new ServiceCollection();
+
+            collection.BindFactoryUi();
+            collection.BindFactoryUiCommand();
+
+            Provider = collection.BuildServiceProvider();
+
+            var uiFactory = Provider.GetService<Func<UiMode, IUserInterface>>();
+
+            UserInterface = uiFactory(UiMode.Cmd);
+        }
+
         private static void Main(string[] args)
         {
-            var ui = InterfaceStorage.GetUi(UiMode.Cmd);
+            UserInterface.Execute("greeting");
         }
     }
 }
