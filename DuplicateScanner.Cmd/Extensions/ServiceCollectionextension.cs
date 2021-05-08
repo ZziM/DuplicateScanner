a6222154d31ea9fb81ce73a0c614d5a64cec9c9e
Adimov, Denis <CSTADIMOD@CORP.JTI.COM>
2021-05-08 13:08:30 +0300

    added DI added command command factory

---
+++ DuplicateScanner.Cmd/Extensions/ServiceCollectionextension.cs
@@ -0,0 +1,16 @@
+﻿using DuplicateScanner.Cmd.UI;
+using DuplicateScanner.Cmd.UI.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DuplicateScanner.Cmd.Extensions
+{
+    public static class ServiceCollectionextension
+    {
+        public static void BindFactoryUi(this ServiceCollection collection)
+            => collection.AddTransient<Func<UiMode, IUserInterface>>(InterfaceFactory.Implementation);
+
+        public static void BindFactoryUiCommand(this ServiceCollection collection)
+            => collection.AddTransient<Func<string, IUiCommand>>(CommandFactory.Implementation);
+    }
+}
---
+++ DuplicateScanner.Cmd/UI/Commands/CommandFactory.cs
@@ -0,0 +1,27 @@
+﻿using DuplicateScanner.Cmd.UI.Commands.Cmd;
+using System;
+using System.Collections.Generic;
+
+namespace DuplicateScanner.Cmd.UI.Commands
+{
+    internal static class CommandFactory
+    {
+        private static readonly Dictionary<string, Func<IServiceProvider, IUiCommand>> Storage = new()
+        {
+            {
+                "greeting",
+                (provider) => new CmdGreeting(provider)
+            }
+        };
+
+        public static Func<string, IUiCommand> Implementation(IServiceProvider provider)
+        {
+            return (key) =>
+            {
+                if (Storage.TryGetValue(key, out var func))
+                    return func(provider);
+                return null;
+            };
+        }
+    }
+}
