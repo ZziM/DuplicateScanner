d71badb314cb080b83f776efdc970da159010b51
Adimov, Denis <CSTADIMOD@CORP.JTI.COM>
2021-05-08 13:05:45 +0300

    renamed

---
+++ DuplicateScanner.Cmd/UI/InterfaceFactory.cs
@@ -0,0 +1,27 @@
+﻿using DuplicateScanner.Cmd.UI.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace DuplicateScanner.Cmd.UI
+{
+    internal static class InterfaceFactory
+    {
+        public static readonly Dictionary<UiMode, Func<IServiceProvider, IUserInterface>> Storage = new()
+        {
+            {
+                UiMode.Cmd,
+                (provider) => new CmdInterface(provider)
+            }
+        };
+
+        public static Func<UiMode, IUserInterface> Implementation(IServiceProvider provider)
+        {
+            return mode =>
+            {
+                if (Storage.TryGetValue(mode, out var func))
+                    return func(provider);
+                return null;
+            };
+        }
+    }
+}
